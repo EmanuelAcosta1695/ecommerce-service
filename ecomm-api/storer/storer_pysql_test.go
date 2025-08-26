14f67d84cfa8b21633b5107a1294fec6541d4b8d
Emanuel <e.acosta1695@gmail.com>
2025-08-26 18:48:23 -0300

    feat: test get product case added

--- ecomm-api/storer/storer_pysql_test.go
+++ ecomm-api/storer/storer_pysql_test.go
@@ -54,3 +54,39 @@ func TestCreateProduct(t *testing.T) {
 	err = mock.ExpectationsWereMet()
 	require.NoError(t, err)
 }
+
+func TestGetProduct(t *testing.T) {
+	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("failed to open sqlmock database: %v", err)
+	}
+	defer mockDB.Close()
+
+	db := sqlx.NewDb(mockDB, "sqlmock")
+	st := NewPySQLStorer(db)
+
+	p := &Product{
+		Name:         "Test Product",
+		Image:        "test_image.jpg",
+		Category:     "Test Category",
+		Description:  "Test Description",
+		Rating:       5,
+		NumReviews:   10,
+		Price:        99.99,
+		CountInStock: 100,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    nil,
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).
+		AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
+
+	mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnRows(rows)
+
+	gp, err := st.GetProduct(context.Background(), 1)
+	require.NoError(t, err)
+	require.Equal(t, int64(1), gp.ID)
+
+	err = mock.ExpectationsWereMet()
+	require.NoError(t, err)
+}
